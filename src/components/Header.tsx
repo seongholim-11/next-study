460cc52cf9b68ebac784e8d66066a59a35006f41
damadu01 <damadu01@gmail.com>
2026-01-19 12:36:28 +0000

    4단계 완료

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,13 @@
+import Link from "next/link";
+
 export default function Header() {
-  return (
-    <header>My Awesome Website</header>
-  );
+    return (
+        <header>
+            My Awesome Website
+            <nav>
+                <Link className="nav-link" href={'/'}>HOME</Link>
+                <Link className="nav-link" href={'/about'}>ABOUT</Link>
+            </nav>
+        </header>
+    );
 }
