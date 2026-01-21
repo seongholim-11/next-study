2e23611f8a599352c8841df6f406aeb5ea085472
damadu01 <damadu01@gmail.com>
2026-01-21 12:38:08 +0000

    10단계 완료

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,30 @@
-'use client'
+// src/components/Header.tsx
+'use client';
 
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import styles from './Header.module.css';
 
-
 export default function Header() {
-    const pathname = usePathname();
+  const pathname = usePathname();
 
-    return (
-        <header className={styles.header}>
-            My Awesome Website
-            <nav className={styles.nav}>
-                <Link className={styles.link} href={'/'}>HOME</Link>
-                <Link className={styles.link}  href={'/blog'}>BLOG</Link>
-            </nav>
-        </header>
-    );
+  return (
+    <header className={styles.header}>
+      My Awesome Blog
+      <nav className={styles.nav}>
+        <Link
+          className={`${styles.link} ${pathname === '/' ? styles.active : ''}`}
+          href="/"
+        >
+          HOME
+        </Link>
+        <Link
+          className={`${styles.link} ${pathname.includes('/blog') ? styles.active : ''}`}
+          href="/blog"
+        >
+          BLOG
+        </Link>
+      </nav>
+    </header>
+  );
 }
